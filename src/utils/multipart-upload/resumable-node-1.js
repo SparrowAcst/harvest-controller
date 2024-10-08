332293ec33e09933fd603f97fe50b849c95e4197
boldak <boldak.andrey@gmail.com>
2024-10-08 18:51:33 +0300

    fix

--- src/utils/multipart-upload/resumable-node-1.js
+++ src/utils/multipart-upload/resumable-node-1.js
@@ -17,7 +17,6 @@ const Resumable = class {
     }
 
     testAllChunkExists(id) {
-        console.log(UPLOAD[id].chunk)
         return keys(UPLOAD[id].chunk)
             .map(key => UPLOAD[id].chunk[key].status)
             .filter(status => status == "done")
@@ -28,64 +27,50 @@ const Resumable = class {
         if (req.busboy) {
             let query = req.query;
             let eventHub = req.eventHub
+            let id = cleanIdentifier(query.resumableIdentifier)
+
+            let chunkMetadata = {
+                file: path.join(this.temporaryFolder, `./resumable-${id}.${query.resumableChunkNumber}`),
+                status: "processed"
+            }
+
+            UPLOAD[id] = UPLOAD[id] || {
+                id,
+                file: `./resumable-${id}.${query.resumableChunkNumber}`,
+                size: query.resumableTotalSize,
+                status: "processed",
+                totalChunks: query.resumableTotalChunks,
+                chunk: {}
+            }
+
+            UPLOAD[id].chunk[chunkMetadata.file] = chunkMetadata
 
             req.busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated) => {
                 if (fieldname) query[fieldname] = val;
             })
 
-            req.busboy.on('file', async (name, file, info) => {
+            req.busboy.on('close', async () => {
+                console.log("busboy.on('finish'...")
+                UPLOAD[id].chunk[chunkMetadata.file].status = "done"
+                UPLOAD[id].chunk[chunkMetadata.file].commpletedAt = new Date()
+                console.log(UPLOAD)
+                if (this.testAllChunkExists(id)) {
+                    UPLOAD[id].status = "done"
+                    UPLOAD[id].completedAt = new Date()
+                    console.log('all chunks ready', UPLOAD[id])
+                    eventHub.emit("resumable-done", UPLOAD[id])
+                    delete UPLOAD[id]
+                }
+            })
+
+            req.busboy.on('file', (name, file, info) => {
                 try {
-                    
-                    console.log("busboy.on('file'", name, file, info)
-                    let id = cleanIdentifier(query.resumableIdentifier)
+                    console.log("busboy.on('file'...")
+                    file.pipe(fs.createWriteStream(chunkMetadata.file))
 
-                    let chunkMetadata = {
-                        file: path.join(this.temporaryFolder, `./resumable-${id}.${query.resumableChunkNumber}`),
-                        status: "processed"
-                    }
-
-                    UPLOAD[id] = UPLOAD[id] || {
-                        id,
-                        file: info.filename,
-                        size: query.resumableTotalSize,
-                        status: "processed",
-                        totalChunks: query.resumableTotalChunks,
-                        chunk: {}
-                    }
-
-                    UPLOAD[id].chunk[chunkMetadata.file] = chunkMetadata
-
-                    let stream = await fs.createWriteStream(chunkMetadata.file) //, { flags: 'w' })
-                    console.log(stream)
-                    stream.on('close', () => {
-                        UPLOAD[id].chunk[chunkMetadata.file].status = "done"
-                        UPLOAD[id].chunk[chunkMetadata.file].commpletedAt = new Date()
-                        console.log("close stream", chunkMetadata.file)
-                        console.log(UPLOAD)
-                        if (this.testAllChunkExists(id)) {
-                            UPLOAD[id].status = "done"
-                            UPLOAD[id].completedAt = new Date()
-                            console.log('all chunks ready', UPLOAD[id])
-                            eventHub.emit("resumable-done", UPLOAD[id])
-                            delete UPLOAD[id]
-                        }
-                    })
-
-                    stream.on('error', e => {
-                        UPLOAD[id].status = "error"
-                        UPLOAD[id].chunk[chunkMetadata.file].status = "error"
-                        UPLOAD[id].chunk[chunkMetadata.file].error = e.toString() + " " + e.stack
-                        eventHub.emit("resumable-error", UPLOAD[id])
-                        delete UPLOAD[id]
-                        console.log(`upload error: ${e.toString()}`)
-                    })
-
-                    file.pipe(stream);
-                
                 } catch (e) {
                     console.log("UPLOAD CHUNK ERROR", id, e.toString(), e.stack)
-                }    
-                // resolve(id)
+                }
             })
 
             req.pipe(req.busboy)
